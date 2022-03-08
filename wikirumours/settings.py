0aab925b02afda1457e21994c4a2c118d6453ad0
Ken Muriithi <muriithiken0@gmail.com>
2022-03-08 09:40:08 +0300

    Update custom settings configuration for production

--- wikirumours/settings.py
+++ wikirumours/settings.py
@@ -1,7 +1,23 @@
 from .base_settings import *
 
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    "wikirumours.org",
+    "www.wikirumours.org",
+    "peacefultruth.org",
+    "www.test.peacefultruth.org",
+    "unahakika.org",
+    "lka.wikirumours.org",
+    "kijijichaamani.org",
+    "hagigawahid.org",
+    "runtuwaanabad.org",
+]
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = "/home/mavericklabs/wikirumours.org/media"
+
+GEOS_LIBRARY_PATH = '/home/mavericklabs/libraries/geos-3.5.2/capi/.libs/libgeos_c.so'
+GDAL_LIBRARY_PATH = '/home/mavericklabs/libraries/gdal-2.4.4/.libs/libgdal.so'
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-from distutils.sysconfig import get_python_lib
-
-os.environ["PATH"] += os.pathsep + get_python_lib() + '\\osgeo'
